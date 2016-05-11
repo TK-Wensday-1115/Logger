4d0286dbfc8c8c995041052382febd94c9f53491
Vanquis <daniel.rodzon@gmail.com>
2016-05-11 02:54:01 +0200

    Component + demo

---
+++ src/logger/component/Logger.java
@@ -0,0 +1,93 @@
+package logger.component;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Logger extends ScrollPane{
+    private final TextArea loggingView = new TextArea();
+    private DateFormat dateFormat = new SimpleDateFormat("[dd.MM.yyyy HH:mm:ss]");
+    private int historySize;
+
+    public Logger(){
+        setupLoggingView();
+        this.setContent(loggingView);
+        this.setFitToWidth(true);
+        this.setFitToHeight(true);
+        this.setPrefWidth(400);
+        this.setPrefHeight(180);
+        this.historySize = 0;
+        setColor(Color.BLACK);
+    }
+
+    private void setupLoggingView() {
+        loggingView.setPrefWidth(1000);
+        loggingView.setPrefHeight(500);
+        loggingView.setWrapText(true);
+        loggingView.setEditable(false);
+    }
+
+    public void append(final String content) {
+        final String message = dateFormat.format(new Date()) + " " + content + "\n";
+        int spaceNeeded = message.length();
+        try {
+            try {
+                if (loggingView != null) {
+                    if (loggingView.getText().length() == 0) {
+                        loggingView.setText(message);
+                    } else if(loggingView.getText().length() + spaceNeeded <= historySize){
+                        loggingView.selectEnd();
+                        loggingView.insertText(loggingView.getText().length(),
+                                message);
+                    }else{
+                        String text = loggingView.getText();
+                        text = text.substring(spaceNeeded);
+                        while(!text.startsWith("\n")){
+                            text = text.substring(1);
+                        }
+                        text = text.substring(1);
+                        loggingView.setText(text);
+                        loggingView.selectEnd();
+                        loggingView.insertText(loggingView.getText().length(),
+                                message);
+                    }
+                }
+            } catch (final Throwable t) {
+                System.out.println("Unable to append log to text area: "
+                        + t.getMessage());
+            }
+        } catch (final IllegalStateException e) {
+            // ignore case when the platform hasn't yet been iniitialized
+        }
+    }
+
+    public void setColor(Color color){
+        loggingView.setStyle("-fx-text-fill: " + toRgbString(color) + ";");
+    }
+
+    private String toRgbString(Color c) {
+        return "rgb("
+                + to255Int(c.getRed())
+                + "," + to255Int(c.getGreen())
+                + "," + to255Int(c.getBlue())
+                + ")";
+    }
+
+    private int to255Int(double d) {
+        return (int) (d * 255);
+    }
+
+    public int getHistorySize() {
+        return historySize;
+    }
+
+    public void setHistorySize(int historySize) {
+        this.historySize = historySize;
+    }
+
+}
---
+++ src/logger/example/DemoApp.java
@@ -0,0 +1,58 @@
+package logger.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import logger.component.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DemoApp extends Application{
+
+    public static void main(String[] args){
+        launch(args);
+    }
+    Thread thread;
+    Logger logger;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        logger = new Logger();
+        Scene scene = new Scene(logger);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        logger.setColor(Color.BROWN);
+        logger.setHistorySize(1000);
+
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+                String line = "";
+                while(!line.equals("q")){
+                    System.out.println("Enter some text: ");
+                    try {
+                        line = buffer.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    logger.append(line);
+                }
+                try {
+                    buffer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        thread.interrupt();
+    }
+}
